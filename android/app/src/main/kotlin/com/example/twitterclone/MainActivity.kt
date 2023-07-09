3cde462142b5972b3b68482648764c0e8c02f506
MuhammadRizqiindra <hudariski578@gmail.com>
2023-07-09 12:30:30 +0700

    WIP: Struktur dan Arsitektur

---
+++ android/app/src/main/kotlin/com/example/twitterclone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kharis_faturrahman.twitter_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '6455f11ee19a678e3b55';
+  static const String projectId = '6423b9ac728639035d7d';
+  static const String endPoint = 'https://baas.pasarjepara.com/v1';
+}
